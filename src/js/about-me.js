b187a00c597fdd7df564c3afdc7dee113cb8500a
Yura Petryna <yur4ik911@gmail.com>
2024-12-22 20:36:55 +0200

    create swiper about

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,19 +1,17 @@
-
 document.addEventListener('DOMContentLoaded', () => {
   const accordionItems = document.querySelectorAll('.accordion-item');
 
   accordionItems.forEach((item, index) => {
     const trigger = item.querySelector('.accordion-trigger');
-    const icon = item.querySelector('.accordion-icon'); 
+    const icon = item.querySelector('.accordion-icon');
 
-    // const svg = icon.querySelector('svg'); 
+    // const svg = icon.querySelector('svg');
 
-  
     if (index === 0) {
       item.classList.add('open');
-      icon.classList.add('active'); 
+      icon.classList.add('active');
 
-      // svg.classList.add('active'); 
+      // svg.classList.add('active');
     }
 
     trigger.addEventListener('click', () => {
@@ -25,19 +23,17 @@ document.addEventListener('DOMContentLoaded', () => {
         const iconToReset = i.querySelector('.accordion-icon');
         const svgToReset = i.querySelector('.accordion-icon svg');
         if (iconToReset) {
-          iconToReset.classList.remove('active'); 
-
+          iconToReset.classList.remove('active');
         }
         // if (svgToReset) {
         //   svgToReset.classList.remove('active');
         // }
-
       });
 
       // відкриваб
       if (!isOpen) {
         item.classList.add('open');
-        icon.classList.add('active'); 
+        icon.classList.add('active');
 
         // svg.classList.add('active');
       }
@@ -46,67 +42,46 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css/navigation';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper-about', {
+    // Конфігурація слайдера
+    slidesPerView: 2, // Кількість видимих слайдів
+    modules: [Navigation, Pagination],
+    navigation: {
+      // nextEl: '.swiper-button-next',
+      nextEl: '.swiper-button-next-2',
+    },
+    loop: true,
+    watchOverflow: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    keyboard: {
+      enabled: true, // Включити керування з клавіатури
+      onlyInViewport: true,
+    },
+    mousewheel: true,
 
-
-////////////////////////////////////////////////////////////////////
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-// import Swiper from 'swiper/bundle';
-// import 'swiper/css/bundle';
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const swiper = new Swiper('.swiper-container', {
-//     // Конфігурація слайдера
-//     slidesPerView: 3, // Кількість видимих слайдів
-//     spaceBetween: 20, // Відстань між слайдами
-//     loop: true, // Циклічний режим
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//     pagination: {
-//       el: '.swiper-pagination',
-//       clickable: true,
-//     },
-
-// slidesPerView: 1,
-// loop: true, // Безкінечний цикл,
-// navigation: {
-//   nextEl: '.swiper-button-next',
-// },
-// keyboard: {
-//   enabled: true,
-//   onlyInViewport: true,
-// },
-// grabCursor: true, // Для зручності на сенсорних пристроях
-//   });
-// });
-// document.addEventListener('DOMContentLoaded', () => {
-//   const swiper = new Swiper('.swiper-about', {
-//     direction: 'horizontal',
-//     loop: true, // Безкінечний цикл
-//     spaceBetween: 20, // Відступи між слайдами
-//     slidesPerView: 'auto', // Автоматична ширина
-//     centeredSlides: true, // Центрування слайдів
-//     pagination: {
-//       el: '.swiper-pagination',
-//       clickable: true,
-//     },
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//     keyboard: {
-//       enabled: true,
-//       onlyInViewport: true,
-//     },
-//     grabCursor: true, // Для зручності на сенсорних пристроях
-//   });
-//   console.log(swiper());
-// });
-
-
-
-
-
+    loopFillGroupWithBlank: true,
+    breakpoints: {
+      // коли ширина >= 640px
+      640: {
+        slidesPerView: 2,
+      },
+      // коли ширина >= 768px
+      768: {
+        slidesPerView: 3,
+      },
+      // коли ширина >= 1024px
+      1440: {
+        slidesPerView: 6,
+      },
+    },
+  });
+});
