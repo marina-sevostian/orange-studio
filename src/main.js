602b684ddb80a9b6a965f441bd615e44eb275478
Marina.S. <marina.sevostian@gmail.com>
2024-12-22 10:13:05 +0200

    modified swiper

--- src/main.js
+++ src/main.js
@@ -9,7 +9,9 @@ import './js/reviews';
 import './js/work-together';
 
 import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/navigation';
 
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
