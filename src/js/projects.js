72169f07b5ab6a7bafb8cc1fd4351cd690fcfa14
MykolaLukianovych <lukjanovych@gmail.com>
2024-12-23 21:18:58 +0200

    finish-button-settings

--- src/js/projects.js
+++ src/js/projects.js
@@ -5,21 +5,51 @@ import 'swiper/css/navigation';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.swiper-projects', {
-  watchOverflow: false,
-  loop: false,
-  direction: 'horizontal',
-  simulateTouch: true,
-  grabCursor: true,
-  slidesPerView: 1,
-  speed: 1000,
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  },
-  navigation: {
-    nextEl: '.button-next',
-    prevEl: '.button-prev',
-  },
+
+  const prevButton = document.querySelector('.button-prev');
+  const nextButton = document.querySelector('.button-next');
+
+  let swiper
+
+  function updateButtonsState() {
+      
+    if (!swiper) return;
+
+    if (swiper.isBeginning) {
+      prevButton.setAttribute('disabled', 'true');
+    } else {
+      prevButton.removeAttribute('disabled');
+    }
+
+    if (swiper.isEnd) {
+      nextButton.setAttribute('disabled', 'true');
+    } else {
+      nextButton.removeAttribute('disabled');
+    }
+  }
+
+  swiper = new Swiper('.swiper-projects', {
+    watchOverflow: false,
+    loop: false,
+    direction: 'horizontal',
+    simulateTouch: true,
+    grabCursor: true,
+    slidesPerView: 1,
+    speed: 1000,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+    },
+    on: {
+      slideChange: updateButtonsState,
+      init: updateButtonsState,
+    },
   });
 });
+
+
+
